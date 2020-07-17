dfb9fcd60fb1b29c34dc8e435252843c0d19c978
jungjin0003 <43538880+jungjin0003@users.noreply.github.com>
2020-07-17 21:33:07 +0900

    Update WinHook.h

--- WinHook.h
+++ WinHook.h
@@ -12,26 +12,29 @@ typedef struct {
 
 int NewMessageBox(HWND hWnd, LPCSTR lpText, LPCSTR lpCation, UINT uType)
 {
-    data *Data = 0xCCCCCCCC;
+    volatile data *Data = 0xCCCCCCCC;
     return ((MESSAGEBOXA)Data->pFunc)(hWnd, Data->Text, Data->Text, uType);
 }
 int AtherFunc() {}
 
 int main()
 {
-    WINAPI_HOOK_DATAA WAHD;
     data Data;
-    scanf("%d", &WAHD.dwPID);
-    WAHD.lpNewFunction = NewMessageBox;
-    WAHD.dwNewFuncSize = (ULONG)AtherFunc - (ULONG)NewMessageBox;
-    WAHD.lpOrigin = MessageBoxA;
-    WAHD.lpCopyOrigin = &Data.pFunc;
-    strcpy(WAHD.DLLName, "user32.dll");
     strcpy(Data.Text, "Hooked!");
-    WAHD.Parameter = TRUE;
-    WAHD.lpParameter = &Data;
-    WAHD.dwParameterSize = sizeof(data);
-    HookA(&WAHD)
+    WINAPI_BASIC_HOOK_DATAA WinApi_Basic_Hook_Data;
+    strcpy(WinApi_Basic_Hook_Data.DLLName, "user32.dll");
+    WinApi_Basic_Hook_Data.lpOrigin = MessageBoxA;
+    WinApi_Basic_Hook_Data.lpNewFunction = NewMessageBox;
+    WinApi_Basic_Hook_Data.lpParameter = &Data;
+    WinApi_Basic_Hook_Data.Parameter = TRUE;
+    WinApi_Basic_Hook_Data.dwParameterSize = sizeof(data);
+    WinApi_Basic_Hook_Data.dwNewFuncSize = (Address)AtherFunc - (Address)NewMessageBox;
+    WinApi_Basic_Hook_Data.lpCopyOrigin = &Data.pFunc;
+
+    // DWORD PID;
+    // scanf("%d", &PID);
+    // HookA(&WinApi_Basic_Hook_Data, NULL, "TEST.exe");
+    // HookA(&WinApi_Basic_Hook_Data, PID, NULL);
 }
 */
 #pragma once
@@ -40,6 +43,7 @@ int main()
 #define _one_is_require_
 #define _require_
 #define _caller_
+#define _option_
 
 #ifdef _M_AMD64
     typedef ULONGLONG Address;
@@ -49,7 +53,32 @@ int main()
     extern const BYTE Instruction[7];
 #endif
 
-typedef struct _WINAPI_HOOK_DATAW {
+typedef struct _WINAPI_BASIC_HOOK_DATAW 
+{
+    PVOID lpOrigin;
+    PVOID *lpCopyOrigin;
+    PVOID lpNewFunction;
+    PVOID lpParameter;
+    BOOL Parameter;
+    DWORD dwParameterSize;
+    DWORD dwNewFuncSize;
+    WCHAR DLLName[MAX_PATH]; 
+} WINAPI_BASIC_HOOK_DATAW, *PWINAPI_BASIC_HOOK_DATAW;
+
+typedef struct _WINAPI_BASIC_HOOK_DATAA
+{
+    PVOID lpOrigin;
+    PVOID *lpCopyOrigin;
+    PVOID lpNewFunction;
+    PVOID lpParameter;
+    BOOL Parameter;
+    DWORD dwParameterSize;
+    DWORD dwNewFuncSize;
+    char DLLName[MAX_PATH]; 
+} WINAPI_BASIC_HOOK_DATAA, *PWINAPI_BASIC_HOOK_DATAA;
+
+typedef struct _WINAPI_HOOK_DATAW 
+{
     HANDLE hProcess;
     HMODULE hModule;
     _require_ PVOID lpOrigin;
@@ -62,13 +91,12 @@ typedef struct _WINAPI_HOOK_DATAW {
     BOOL Parameter;
     DWORD dwParameterSize;
     _require_ DWORD dwNewFuncSize;
-    _one_is_require_ DWORD dwPID;
     BYTE jmpCode[sizeof(Instruction)];
     _require_ WCHAR DLLName[MAX_PATH]; 
-    _one_is_require_ WCHAR ProcessName[MAX_PATH];
 } WINAPI_HOOK_DATAW, *PWINAPI_HOOK_DATAW;
 
-typedef struct _WINAPI_HOOK_DATAA {
+typedef struct _WINAPI_HOOK_DATAA 
+{
     HANDLE hProcess;
     HMODULE hModule;
     _require_ PVOID lpOrigin;
@@ -81,13 +109,13 @@ typedef struct _WINAPI_HOOK_DATAA {
     BOOL Parameter;
     DWORD dwParameterSize;
     _require_ DWORD dwNewFuncSize;
-    _one_is_require_ DWORD dwPID;
     BYTE jmpCode[sizeof(Instruction)];
     _require_ char DLLName[MAX_PATH]; 
-    _one_is_require_ char ProcessName[MAX_PATH];
 } WINAPI_HOOK_DATAA, *PWINAPI_HOOK_DATAA;
 
 #ifdef UNICODE
+    typedef WINAPI_BASIC_HOOK_DATAW WINAPI_BASIC_HOOK_DATA;
+    typedef PWINAPI_BASIC_HOOK_DATAW PWINAPI_BASIC_HOOK_DATA;
     typedef WINAPI_HOOK_DATAW WINAPI_HOOK_DATA;
     typedef PWINAPI_HOOK_DATAW PWINAPI_HOOK_DATA;
     #define Hook HookW
@@ -97,7 +125,10 @@ typedef struct _WINAPI_HOOK_DATAA {
     #define SetCopyFunction SetCopyFunctionW
     #define WriteParameter WriteParameterW
     #define CodePatch CodePatchW
+    #define Set_WINAPI_Struct Set_WINAPI_StructW;
 #else
+    typedef WINAPI_BASIC_HOOK_DATAA WINAPI_BASIC_HOOK_DATA;
+    typedef PWINAPI_BASIC_HOOK_DATAA PWINAPI_BASIC_HOOK_DATA;
     typedef WINAPI_HOOK_DATAA WINAPI_HOOK_DATA;
     typedef PWINAPI_HOOK_DATAA PWINAPI_HOOK_DATA;
     #define Hook HookA
@@ -107,23 +138,26 @@ typedef struct _WINAPI_HOOK_DATAA {
     #define SetCopyFunction SetCopyFunctionA
     #define WriteParameter WriteParameterA
     #define CodePatch CodePatchA
+    #define Set_WINAPI_Struct Set_WINAPI_StructA;
 #endif
 
-HANDLE GetProcessHandleByFileNameA(char*);
-HANDLE GetProcessHandleByFileNameW(WCHAR*);
-BOOL HookA(PWINAPI_HOOK_DATAA);
-BOOL HookW(PWINAPI_HOOK_DATAW);
-BOOL WriteNewFunctionA(PWINAPI_HOOK_DATAA);
-BOOL WriteNewFunctionW(PWINAPI_HOOK_DATAW);
-BOOL CopyDLLCodeA(PWINAPI_HOOK_DATAA);
-BOOL CopyDLLCodeW(PWINAPI_HOOK_DATAW);
-BOOL SetAssemblyInstructionA(PWINAPI_HOOK_DATAA);
-BOOL SetAssemblyInstructionW(PWINAPI_HOOK_DATAW);
-BOOL SetCopyFunctionA(PWINAPI_HOOK_DATAA);
-BOOL SetCopyFunctionW(PWINAPI_HOOK_DATAW);
-BOOL WriteParameterA(PWINAPI_HOOK_DATAA);
-BOOL WriteParameterW(PWINAPI_HOOK_DATAW);
-BOOL CodePatchA(PWINAPI_HOOK_DATAA);
-BOOL CodePatchW(PWINAPI_HOOK_DATAW);
+HANDLE GetProcessHandleByFileNameA(char* name);
+HANDLE GetProcessHandleByFileNameW(WCHAR* name);
+BOOL HookA(PWINAPI_BASIC_HOOK_DATAA lpWinApi_Basic_Hook_Data, DWORD PID, char *ProcessName);
+BOOL HookW(PWINAPI_BASIC_HOOK_DATAW lpWinApi_Basic_Hook_Data, DWORD PID, wchar_t *ProcessName);
+BOOL WriteNewFunctionA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data);
+BOOL WriteNewFunctionW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data);
+BOOL CopyDLLCodeA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data);
+BOOL CopyDLLCodeW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data);
+BOOL SetAssemblyInstructionA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data);
+BOOL SetAssemblyInstructionW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data);
+BOOL SetCopyFunctionA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data);
+BOOL SetCopyFunctionW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data);
+BOOL WriteParameterA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data);
+BOOL WriteParameterW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data);
+BOOL CodePatchA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data);
+BOOL CodePatchW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data);
+BOOL Set_WINAPI_StructA(PWINAPI_HOOK_DATAA lpWinApi_Hook_Data, PWINAPI_BASIC_HOOK_DATAA lpWinApi_Basic_Hook_Data);
+BOOL Set_WINAPI_StructW(PWINAPI_HOOK_DATAW lpWinApi_Hook_Data, PWINAPI_BASIC_HOOK_DATAW lpWinApi_Basic_Hook_Data);
 
 #endif
